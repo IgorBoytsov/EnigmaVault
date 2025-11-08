900003a7351403df2a5e7765172caf78fdac1430
Light Play <lightplay456@gmail.com>
2025-11-08 16:13:41 +0300

    feat(utils): добавить методы-расширения для коллекций
    Добавлены утилитарные методы-расширения для работы с коллекциями:
    - Load: для эффективного наполнения ICollection<T> из IEnumerable<T>.
    - ToObservableCollection: для удобного преобразования IEnumerable<T> в ObservableCollection<T>, что часто требуется в MVVM.

---
+++ dotnet/src/apps/EnigmaVault.Desktop/Helpers/CollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace EnigmaVault.Desktop.Helpers
+{
+    internal static class CollectionExtensions
+    {
+        public static void Load<T>(this ICollection<T> source, IEnumerable<T> data,bool isClear = false)
+        {
+            if (isClear)
+                source.Clear();
+
+            foreach (var item in data)
+                source.Add(item);
+        }
+
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> data)
+        {
+            var obs = new ObservableCollection<T>();
+
+            foreach (var item in data)
+                obs.Add(item);
+
+            return obs;
+        }
+    }
+}
