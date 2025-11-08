a33484bb458806036c099fdbc5278b2d9f5c6939
Light Play <lightplay456@gmail.com>
2025-11-08 16:26:07 +0300

    feat(ui-wpf): добавить набор базовых поведений для UI-элементов
    Добавлены переиспользуемые поведения для расширения стандартной функциональности элементов управления и окон:
    
    - AsyncInitializeBehavior: Обеспечивает асинхронную инициализацию ViewModel при загрузке View.
    - InvokeCommandWithHandled: Позволяет вызывать команду и помечать событие (например, MouseDown) как обработанное, чтобы предотвратить его дальнейшее распространение.
    - TabControlWheelScrollBehavior: Добавляет возможность прокрутки вкладок в TabControl с помощью колеса мыши.
    - WindowDragBehavior: Позволяет перетаскивать окно за любой элемент, к которому применено это поведение, а не только за заголовок.

---
+++ dotnet/src/apps/EnigmaVault.Desktop/Resources/Behaviors/AsyncInitializeBehavior.cs
@@ -0,0 +1,29 @@
+﻿using EnigmaVault.Desktop.ViewModels.Base;
+using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace EnigmaVault.Desktop.Resources.Behaviors
+{
+    public class AsyncInitializeBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += OnLoaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= OnLoaded;
+            base.OnDetaching();
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject.DataContext is IAsyncInitializable viewModel)
+            {
+                await viewModel.InitializeAsync();
+            }
+        }
+    }
+}
