f8bc9b733afcd1f830b5322c0ad66fc9f05dc9c6
Light Play <lightplay456@gmail.com>
2025-12-09 13:39:22 +0300

    feat(ui): добавлен компонент PlaceHolderTextBox
    Реализован новый контрол, наследуемый от TextBox, с поддержкой текста-подсказки.
    
    Изменения:
    - Создан класс PlaceHolderTextBox с DependencyProperty для PlaceHolder и CornerRadius.
    - Добавлен стиль и ControlTemplate для нового компонента.
    - Реализована логика отображения плейсхолдера (через триггер на пустое свойство Text).
    - Добавлены визуальные стили для состояний наведения и фокуса.

---
+++ dotnet/src/apps/EnigmaVault.Desktop/Views/Controls/PlaceHolderTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EnigmaVault.Desktop.Views.Controls
+{
+    public class PlaceHolderTextBox : TextBox
+    {
+        public string PlaceHolder
+        {
+            get => (string)GetValue(PlaceHolderProperty);
+            set => SetValue(PlaceHolderProperty, value);
+        }
+
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(PlaceHolderTextBox), new PropertyMetadata(default));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(PlaceHolderTextBox), new PropertyMetadata(default));
+
+        static PlaceHolderTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceHolderTextBox), new FrameworkPropertyMetadata(typeof(PlaceHolderTextBox)));
+        }
+    }
+}
