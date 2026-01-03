c9d83b6f231ab04d5f24d2ad577b02801f1402a6
Light Play <lightplay456@gmail.com>
2026-01-03 09:24:42 +0300

    feat(desktop): добавить ControlHelper для расширенного управления UI
    - Создан класс ControlHelper с присоединенными свойствами.
    - Добавлено свойство RightClickCommand для обработки нажатия правой кнопки мыши через MVVM-команды.
    - Добавлено свойство CornerRadius для передачи параметров скругления углов в шаблоны через стили.

---
+++ dotnet/src/apps/EnigmaVault.Desktop/Resources/Attachable/ControlHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EnigmaVault.Desktop.Resources.Attachable
+{
+    public static class ControlHelper
+    {
+        #region Клик на ПКМ
+
+        public static readonly DependencyProperty RightClickCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "RightClickCommand",
+                typeof(ICommand),
+                typeof(ControlHelper), 
+                new PropertyMetadata(null));
+
+        public static void SetRightClickCommand(DependencyObject element, ICommand value) => element.SetValue(RightClickCommandProperty, value);
+
+        public static ICommand GetRightClickCommand(DependencyObject element) => (ICommand)element.GetValue(RightClickCommandProperty);
+
+        #endregion
+
+        #region Скругление
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "CornerRadius", 
+                typeof(CornerRadius), 
+                typeof(ControlHelper), 
+                new PropertyMetadata(new CornerRadius(0)));
+
+        public static void SetCornerRadius(DependencyObject element, CornerRadius value) => element.SetValue(CornerRadiusProperty, value);
+
+        public static CornerRadius GetCornerRadius(DependencyObject element) => (CornerRadius)element.GetValue(CornerRadiusProperty);
+
+        #endregion
+    }
+}
